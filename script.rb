d2b970b22f18f15bb96ca5af9fd3f7106f6eaf84
Leonidas Tsiakas <leonidastsiakas13@gmail.com>
2023-06-13 14:05:38 +0300

    Turn non shifted numbers back to characters

--- script.rb
+++ script.rb
@@ -1,7 +1,6 @@
 def caesar_cipher (string, shift_factor)
   nums = string.split('').map { |i| i.ord}
-  new_arr = nums.map do |num|
-    
+  new_arr = nums.map do |num|    
       if num.between?(65, 90)
         new_num = num + shift_factor
           if new_num > 90
@@ -16,10 +15,9 @@ def caesar_cipher (string, shift_factor)
             final_char = final_num.chr
           else final_char = new_num.chr
           end
-      else num
-      end
-  
+      else num.chr
+      end  
   end
   new_arr.join('')
 end
-caesar_cipher("What a string!", 5)
+
